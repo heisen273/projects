e13f8b4eea63d663613dfdd13fd97fa133c4df5c
Anton Antonenko <f0xkid@ukr.net>
2015-12-18 09:39:55 +0200

    OK THIS IS BEST UPDATE EVER U"LL LOVE IT JUST TRY

--- battleships.py
+++ battleships.py
@@ -1,48 +1,69 @@
+import random
 import sys
-from random import randint
-board = []
 print(chr(27) + "[2J")
+board = []
+ships=[]
+
 for x in range(0, 10):
     board.append((["O"]) * 10)
+for x in range(0, 10):
+    ships.append(([0]) * 10)
 
 def print_board(board):
-    for row in board:
-        print " ".join(row)
+	for row in board:
+		print " ".join(row)
 
+def print_ships(board):
+	for row in board:
+		print ' '.join(str(e) for e in row)
 print_board(board)
 
-def random_row(board):
-    return randint(0, len(board) - 1)
-
-def random_col(board):
-    return randint(0, len(board) - 1)
-    
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-n=[0,1,2,3,4,5,6,7,8,9]
-for turn in range(10):
-		print "GUESS MUST BE BETWEEN 0 AND 9"
-		guess_row = int(raw_input("Guess Row:"))
-		guess_col = int(raw_input("Guess Col:"))
-		print "GUESS MUST BE BETWEEN 0 AND 9"
-		sys.stderr.write("\x1b[2J\x1b[H")
-		print ship_row
-		print ship_col
-# Write your code below!
-		if guess_row == ship_row and guess_col == ship_col:
-		  	print "Congratulations! You sank my battleship!"
-			board[guess_row][guess_col]="\033[1;31mX\033[1;m"
-			print_board(board)
-			break
-		elif (guess_row>10 or guess_row<0) or (guess_col>10 or guess_col<0):
-			print "WRONG NUMBER, FELLA"
-		elif board[guess_row][guess_col]=="X":
-			print "You've guessed this one already!"
+#def random_row(board):
+#    return randint(0, len(board) - 1)
+#
+#
+#def random_col(board):
+#    return randint(0, len(board) - 1)
+#
+def random_dot():
+	l=random.sample(range(0,99),8)
+	for item in l:
+		if item < 10:
+			ship_row=0
+			ship_col=item
 		else:
-    			print "You missed my battleship!"
-			board[guess_row][guess_col]="X"
-   			print_board(board)
-			if turn==9:
-				print "GAME OVER, no more turns"
-		print "Turn",turn+1
+			ship_row=item//10
+			ship_col=item%10
+	return [ship_row,ship_col]	
+dot=[]
+for i in range(0,8):
+	dot=random_dot()
+	ships[dot[0]][dot[1]]=1
+print "####################################"
+print_ships(ships)
+print dot
+for turn in range(10):
+	print "GUESS MUST BE BETWEEN 0 AND 9"
+	guess_row = int(raw_input("Guess Row:"))
+	guess_col = int(raw_input("Guess Col:"))
+	print "GUESS MUST BE BETWEEN 0 AND 9"
+	sys.stderr.write("\x1b[2J\x1b[H")
+	print_ships(ships)
+
+        if ships[guess_row][guess_col]==1:
+                print "Congratulations! You sank my battleship!"
+                board[guess_row][guess_col]="\033[1;31mX\033[1;m"
+                print_board(board)
+#                break
+        elif (guess_row>10 or guess_row<0) or (guess_col>10 or guess_col<0):
+                print "WRONG NUMBER, FELLA"
+        elif board[guess_row][guess_col]=="X":
+                print "You've guessed this one already!"
+        else:
+                print "You missed my battleship!"
+                board[guess_row][guess_col]="X"
+                print_board(board)
+                if turn==9:
+                        print "GAME OVER, no more turns"
+        print "Turn",turn+1
+                                     
