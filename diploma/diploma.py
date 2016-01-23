9ec1840b167c2645f6828100b99f2b1f7a7e09a9
anton <f0xkid@ukr.net>
2016-01-23 21:06:12 +0200

    this is huge, man

--- diploma/diploma.py
+++ diploma/diploma.py
@@ -1,92 +1,137 @@
 #!/usr/bin/env python
+from __future__ import division
 from fractions import Fraction
 import matplotlib.pyplot as plt
+import wx
 import FileDialog
-import Tkinter
+from Tkinter import *
 import numpy
 
 
-def isfraction(input):
-    try:
-        return Fraction(input) != None;
-    except ValueError:
-        return False;
-def isfloat(input):
-    try:
-        return float(input) != None;
-    except ValueError:
-        return False;
+class app():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('app')
+        self.mass_string = StringVar()
+        mass = Entry(self.window, textvariable=self.mass_string)
+        mass.grid(row=0, column=1, columnspan=2)
 
-def diploma():
+        mass_labeltext = StringVar()
+        mass_labeltext.set('Enter SiHx mass in gramms:')
+        mass_label = Label(self.window, textvariable=mass_labeltext, height=1)
+        mass_label.grid(row=0, column=0)
 
-    print 'SiHx + 2 H2O = SiO2+ (2+x/2) H2'
-    mass_boolean = False
-    x_boolean = False
+        self.x_string = StringVar()
+        x = Entry(self.window, textvariable=self.x_string)
+        x.grid(row=1, column=1, columnspan=10)
 
-    while mass_boolean != True:
-        mass = raw_input('Enter SiHx mass in gramms: ')
-        if isfraction(mass) == True:
-            mass = Fraction(mass)
-            if mass > 0:
-                mass_boolean = True
-            else:
-                mass_boolean = False
-        elif isfloat(mass) == True:
-            mass_boolean = True
-        else:
-            print 'Please, enter valid value'
-            mass_boolean=False
-    while x_boolean != True:
-        x = raw_input('Enter x in range [0,3]: ')
-        if x.isdigit() and int(x) in range(4):
-            x_boolean = True
+        x_labeltext = StringVar()
+        x_labeltext.set('Enter x in range [0,3]:')
+        x_label = Label(self.window, textvariable=x_labeltext, height=1)
+        x_label.grid(row=1, column=0)
 
-    result0 = (2 * 22.4 * float(mass)) / (28)
-    result1 = (2.5 * 22.4 * float(mass)) / (29)
-    result2 = (3 * 22.4 * float(mass)) / (30)
-    result3 = (3.5 * 22.4 * float(mass)) / (31)
-    print ''
-    print ''
-    if int(x) == 0:
-        print ''
-        print "Si + 2H2O = SiO2 + 2H2 "
-        print result0, "gramms of Hydrogen was allocated"
+        self.output = Text(self.window, height=10, width=60, highlightbackground='black')
+        self.output.grid(row=4, column=0)
 
-    elif int(x) == 1:
-        print ''
-        print "2SiH + 4H2O = 2SiO2 + 5H2 "
-        print result1, "gramms of Hydrogen was allocated"
+        output_labeltext = StringVar()
+        output_labeltext.set('Program Output')
+        output_label = Label(self.window, textvariable=output_labeltext, height=1)
+        output_label.grid(row=3, column=0)
+        values = ['quit', 'plot', 'clear', 'close']
 
-    elif int(x) == 2:
-        print ''
-        print "SiH2 + 2 H2O = SiO2 + 3H2"
-        print result2, "gramms of Hydrogen was allocated"
-    elif int(x) == 3:
-        print ''
-        print "2SiH3 + 4H2O = 2SiO2 + 7H2"
-        print result3, "gramms of Hydrogen was allocated"
-    plt.plot([result0, result1, result2, result3], [0, 1, 2, 3], 'ro-')
-    plt.axis([0, result3 + 200, 0, 4])
-    plt.ylabel('X')
-    plt.xlabel('AMOUNT')
-    plt.show()
+        for txt in values:
+            padx = 6
+            pady = 5
+
+            if txt == 'clear':
+                btn = Button(self.window, height=2, width=4, padx=padx, pady=pady, text=txt,
+                             command=lambda: self.clear())
+                btn.grid(row=3, column=2, padx=1, pady=1)
+            elif txt == 'close':
+                btn = Button(self.window, height=2, width=5, padx=padx, pady=pady, text=txt,
+                             command=lambda: self.close())
+                btn.grid(row=2, column=2, padx=1, pady=1)
+            elif txt == 'plot':
+                btn = Button(self.window, height=2, width=4, padx=padx, pady=pady, text=txt,
+                             command=lambda: self.plot(mass))
+                btn.grid(row=2, column=1, padx=1, pady=1)
+            elif txt == 'quit':
+                btn = Button(self.window, height=2, width=4, padx=padx, pady=pady, text=txt,
+                             command=lambda: self.quit())
+                btn.grid(row=3, column=1, padx=1, pady=1)
+        self.output.insert(INSERT, 'SiHx + 2 H2O = SiO2+ (2+x/2) H2')
+        self.window.mainloop()
+
+    def quit(self):
+        self.window.destroy()
+
+    def clear(self):
+        self.mass_string.set('')
+
+        self.x_string.set('')
+        plt.clf()
+
+    def close(self):
+        plt.close('all')
+
+    def plot(self, mass):
+        try:
+            mass = Fraction(self.mass_string.get())
+
+            if int(mass) < 0:
+                raise Exception
+        except Exception:
+            self.mass_string.set('error')
+
+        try:
+            x = self.x_string.get()
+
+            if x.isdigit() is not True or int(x) not in range(4):
+                raise Exception
+
+        except Exception:
+            self.x_string.set('error')
+            return False;
 
 
 
+        result0 = (2 * 22.4 * mass) / (28)
+        result1 = (2.5 * 22.4 * mass) / (29)
+        result2 = (3 * 22.4 * mass) / (30)
+        result3 = (3.5 * 22.4 * mass) / (31)
+
+        if int(x) == 0:
+            self.output.insert(INSERT, '\nx ')
+            self.output.insert(INSERT, '= 0')
+            self.output.insert(INSERT, "\n\nSi + 2H2O = SiO2 + 2H2 \n\n")
+            self.output.insert(INSERT, result0)
+            self.output.insert(INSERT, " liters of Hydrogen was allocated")
+        elif int(x) == 1:
+            self.output.insert(INSERT, '\nx ')
+            self.output.insert(INSERT, '= 1')
+            self.output.insert(INSERT, "\n\n2SiH + 4H2O = 2SiO2 + 5H2 \n\n")
+            self.output.insert(INSERT, result1)
+            self.output.insert(INSERT, " liters of Hydrogen was allocated")
+        elif int(x) == 2:
+            self.output.insert(INSERT, '\nx ')
+            self.output.insert(INSERT, '= 2')
+            self.output.insert(INSERT, "\n\nSiH2 + 2 H2O = SiO2 + 3H2 \n\n")
+            self.output.insert(INSERT, result2)
+            self.output.insert(INSERT, " liters of Hydrogen was allocated")
+        elif int(x) == 3:
+            self.output.insert(INSERT, '\nx ')
+            self.output.insert(INSERT, '= 3')
+            self.output.insert(INSERT, "\n\n2SiH3 + 4H2O = 2SiO2 + 7H2 \n\n")
+            self.output.insert(INSERT, result3)
+            self.output.insert(INSERT, " liters of Hydrogen was allocated")
+
+        plt.ion()
+        plt.plot([result0, result1, result2, result3], [0, 1, 2, 3], 'ro-')
+        plt.axis([0, result3 + 200, 0, 4])
+        plt.ylabel('X')
+        plt.xlabel('AMOUNT')
+        plt.autoscale(enable=True, axis='both', tight=None)
+        plt.show()
 
 
-diploma()
-
-def repeat():
-    repeating = True
-    while repeating is True:
-        print 'Repeat calculations with different values?'
-        decision = raw_input('y/n?')
-        if decision == 'n':
-            print 'OK, terminating . . .'
-            repeating = False
-        elif decision == 'y':
-            diploma()
-        else:
-            print 'Your decision must be either \"y\" or \"n\".'
-repeat()
+app()
